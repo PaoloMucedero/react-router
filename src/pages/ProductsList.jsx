8f4326de219b97787eb0c072bbe2fe6d8e87e411
PaoloMucedero <p.mucedero@gmail.com>
2026-01-31 20:35:43 +0100

    Importate tutte le dipendenze; aggiunta funzione per chiamare API esterna

--- src/pages/ProductsList.jsx
+++ src/pages/ProductsList.jsx
@@ -4,16 +4,31 @@ const endpoint = "https://fakestoreapi.com/products"
 
 
 function ProductsList() {
-  
+
+  // funzione che chiama endpoint API
+  function fetchProducts() {
+        axios.get(endpoint)
+            .then((res) => console.log(res.data))
+            .catch(err => console.error("errore richiesta"))
+  }
+  fetchProducts()
 
   return (
     <>
-        <NavBar />
-            <h2>ciao sei su LISTA PRODOTTI</h2>
-            <p>
-                Contenuto della pagina
-                dslgdsgdsigsdpdssdsdò
-            </p>
+      <NavBar />
+      <h2>ciao sei su LISTA PRODOTTI</h2>
+      <div className="row-flex">
+        <div className="card">
+          <div className="top">
+            <img src="null" alt="Maglietta-rel-fit-tee" />
+          </div>
+          <div className="bottom">
+            <figcaption className="brand">Levi's</figcaption>
+            <p className="article-name">RELAXED FIT TEE UNISEX</p>
+            <div className="prezzi"></div>
+          </div>
+        </div>
+      </div>
 
 
     </>
