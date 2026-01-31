9ab0b3e0b2d4249cc1b5f29610c5aef7d8982ec4
PaoloMucedero <p.mucedero@gmail.com>
2026-01-31 21:48:15 +0100

    Implementata lista prodotti

--- src/pages/ProductsList.jsx
+++ src/pages/ProductsList.jsx
@@ -1,33 +1,45 @@
 import axios from "axios"
 import NavBar from "../components/NavBar"
+import { useState, useEffect } from "react"
+
+
+
 const endpoint = "https://fakestoreapi.com/products"
 
 
 function ProductsList() {
+  // variabile stato prodotti
+  const [products, setProducts] = useState([]);
+
 
   // funzione che chiama endpoint API
   function fetchProducts() {
-        axios.get(endpoint)
-            .then((res) => console.log(res.data))
-            .catch(err => console.error("errore richiesta"))
+    axios.get(endpoint)
+      .then((res) => setProducts(res.data))
+      .catch(err => console.error("errore richiesta"))
   }
-  fetchProducts()
+
+
+  useEffect(() => {
+    fetchProducts()
+  }, []);
 
   return (
     <>
       <NavBar />
       <h2>ciao sei su LISTA PRODOTTI</h2>
       <div className="row-flex">
-        <div className="card">
-          <div className="top">
-            <img src="null" alt="Maglietta-rel-fit-tee" />
+        {products.map((product) => (
+          <div key={product.id} className="card">
+            <img className="card-img-top" src={product.image} alt="" />
+            <p>{product.category}</p>
+            <p>{product.title}</p>
+            <p>{product.description}</p>
+            <p>{product.price} €</p>
+            <p>Voto: {product.rating?.rate}</p>
+            <p>Recensioni: {product.rating?.count}</p>
           </div>
-          <div className="bottom">
-            <figcaption className="brand">Levi's</figcaption>
-            <p className="article-name">RELAXED FIT TEE UNISEX</p>
-            <div className="prezzi"></div>
-          </div>
-        </div>
+        ))}
       </div>
 
 
