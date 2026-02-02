7c13f1fa64e7f937efc55da2d570f88e321dfd0d
PaoloMucedero <p.mucedero@gmail.com>
2026-02-02 08:45:38 +0100

    Refactoring per layout definitivo ProductsList.jsx

--- src/pages/ProductsList.jsx
+++ src/pages/ProductsList.jsx
@@ -26,21 +26,40 @@ function ProductsList() {
 
   return (
     <>
-      {products.map((product) => (
-        <Link
-          key={product.id}
-          to={`/products/${product.id}`}
-          style={{ textDecoration: "none", color: "inherit" }}
-        >
-          <div className="card">
-            <img className="card-img-top" src={product.image} alt={product.title} />
-            <p>{product.category}</p>
-            <p>{product.title}</p>
-            <p>{product.price} €</p>
-          </div>
-        </Link>
-      ))}
 
+      <div className="container mt-4">
+        <div className="row g-4">
+          {products.map(product => (
+            <div
+              key={product.id}
+              className="col-12 col-sm-6 col-md-4 col-lg-3"
+            >
+              <Link
+                to={`/products/${product.id}`}
+                className="text-decoration-none text-dark"
+              >
+                <div className="card h-100 shadow-sm">
+                  <img
+                    src={product.image}
+                    className="card-img-top p-3"
+                    style={{ height: "200px", objectFit: "contain" }}
+                    alt={product.title}
+                  />
+
+                  <div className="card-body d-flex flex-column">
+                    <h6 className="card-title">{product.title}</h6>
+                    <p className="text-muted">{product.category}</p>
+
+                    <div className="mt-auto">
+                      <strong>{product.price} €</strong>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
 
     </>
   )
