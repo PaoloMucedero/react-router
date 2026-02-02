6b990c53a4ed8e0863816ddae4fc91cdda072328
PaoloMucedero <p.mucedero@gmail.com>
2026-02-02 08:32:46 +0100

    implementato tasto per tornare alla lista prodotti

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,31 +1,38 @@
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 function ProductDetail() {
-  const { id } = useParams(); // leggiamo l'id dalla URL
+  const { id } = useParams();
+  const navigate = useNavigate();
   const [product, setProduct] = useState(null);
 
   useEffect(() => {
     axios
       .get(`https://fakestoreapi.com/products/${id}`)
       .then(res => setProduct(res.data))
-      .catch(err => console.error("Prodotto non trovato"));
+      .catch(err => console.error("Errore"));
   }, [id]);
 
-  if (!product) {
-    return <p>Caricamento...</p>;
-  }
+  if (!product) return <p>Caricamento...</p>;
 
   return (
     <div className="container">
+      <button
+        className="btn btn-secondary mb-3"
+        onClick={() => navigate("/prodotti")}
+      >
+        ← Torna ai prodotti
+      </button>
+
       <h2>{product.title}</h2>
       <img src={product.image} alt={product.title} style={{ width: "200px" }} />
       <p>{product.description}</p>
-      <p><strong>Categoria:</strong> {product.category}</p>
-      <p><strong>Prezzo:</strong> {product.price} €</p>
+      <p>{product.price} €</p>
     </div>
   );
 }
+
 
 export default ProductDetail;
